2ecfa23cd3027044312524733549e7edc4a44da3
saulocaceresg <scaceresg@unsa.edu.pe>
2025-10-27 16:58:13 -0500

    Subiendo el Ejercicio 5

---
+++ Ejercicio5.js
@@ -0,0 +1,7 @@
+function Promedio(){
+    let nota1 = parseFloat(prompt("Ingresa la nota 1"));
+    let nota2 = parseFloat(prompt("Ingresa la nota 2"));
+    let nota3 = parseFloat(prompt("Ingresa la nota 3"));
+    let promedio = (nota1+nota2+nota3)/3;
+    console.log("El promedio es:" + promedio);
+}
