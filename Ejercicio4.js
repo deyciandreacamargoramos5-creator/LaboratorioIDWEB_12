5a6362ba083381d176c27be4883b52a21517733b
saulocaceresg <scaceresg@unsa.edu.pe>
2025-10-27 16:52:27 -0500

    Subiendo el Ejercicio 4

---
+++ Ejercicio4.js
@@ -0,0 +1,7 @@
+function mayorDeTres(){
+    let a =parseFloat(prompt("Ingrese el valor de a"));
+    let b =parseFloat(prompt("Ingrese el valor de b"));
+    let c =parseFloat(prompt("Ingrese el valor de c"));
+    let mayor = Math.max(a,b,c);
+    console.log("El valor mayor es:" + mayor);
+}
